4f7a1f8673cab6d8e182ac02c78bfc980d58e00d
= <=>
2023-04-03 02:21:43 -0600

    Actualizar demo con manejo de graficos

--- demo/src/main/java/com/example/demo/CanvasController.java
+++ demo/src/main/java/com/example/demo/CanvasController.java
@@ -15,35 +15,44 @@ import java.util.ResourceBundle;
 
 public class CanvasController implements Initializable {
     @FXML
-    private AnchorPane anchorPane;
-
-    GraphicsContext g;
     Canvas canvas;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        canvas = new Canvas(300, 300);
-        anchorPane = new AnchorPane(canvas);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext graphicsContext2D = canvas.getGraphicsContext2D();
 
-        gc.setFill( Color.BLUE );
-        gc.fillRect(0, 0, 100, 100);
-    }
+        // Crear un rectangulo rojo
+        graphicsContext2D.setFill(Color.valueOf("#ff0000"));
+        graphicsContext2D.fillRect(100, 100, 200, 200);
 
-    @FXML
-    public void mouseClick(MouseEvent evt) {
-        System.out.printf("x=%f, y=%f%n", evt.getX(), evt.getY());
+        // Crear un rectangulo azul
+        graphicsContext2D.setStroke(Color.valueOf("#0000ff"));
+        graphicsContext2D.strokeRect(200, 200, 200, 200);
 
-        System.out.println("Mouse clicked");
-        /*if ( evt.isShiftDown() ) {
-            System.out.println("blue");
-            g.setFill( Color.BLUE );
-            g.fillOval( evt.getX() - 30, evt.getY() - 15, 60, 30 );
-        }
-        else {
-            System.out.println("red");
-            g.setFill( Color.RED );
-            g.fillRect( evt.getX() - 30, evt.getY() - 15, 60, 30 );
-        }*/
+        graphicsContext2D.setStroke(Color.valueOf("#35ed0c"));
+        graphicsContext2D.strokeOval(100, 100, 300, 300);
+
+        graphicsContext2D.setStroke(Color.BLACK);
+        graphicsContext2D.strokeLine(100, 100, 400, 400);
+
+        graphicsContext2D.setFill(Color.BLACK);
+        graphicsContext2D.fillPolygon(
+                new double[] {0, 100, 200, 100, 0},
+                new double[] {200, 100, 100, 200, 200},
+                4
+        );
+        canvas.setOnMousePressed( evt -> {
+            GraphicsContext g = canvas.getGraphicsContext2D();
+            double red = evt.getY() / canvas.getHeight();
+            double blue = evt.getX() / canvas.getWidth();
+            double green = Math.random();
+            if (evt.isShiftDown()) {
+                g.setFill(Color.color(red, green, blue));
+                g.fillOval(evt.getX(), evt.getY(), 60, 30);
+            } else {
+                g.setFill(Color.color(red, green, blue));
+                g.fillRect(evt.getX(), evt.getY(), 60, 30);
+            }
+        });
     }
 }
--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -14,64 +14,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        drawStart(stage);
-    }
-
-    private void simpleStart(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("canvas-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 450, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 512, 512);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    private void drawStart(Stage stage) throws IOException {
-        final int WIDTH = 512;
-        final int HEIGHT = 512;
-        // Objeto Canvas
-        Canvas canvas = new Canvas();
-        canvas.setWidth(WIDTH);
-        canvas.setHeight(HEIGHT);
-
-        GraphicsContext graphicsContext2D = canvas.getGraphicsContext2D();
-
-        // Crear un rectangulo rojo
-        graphicsContext2D.setFill(Color.valueOf("#ff0000"));
-        graphicsContext2D.fillRect(100, 100, 200, 200);
-
-        // Crear un rectangulo azul
-        graphicsContext2D.setStroke(Color.valueOf("#0000ff"));
-        graphicsContext2D.strokeRect(200, 200, 200, 200);
-
-        graphicsContext2D.setStroke(Color.valueOf("#35ed0c"));
-        graphicsContext2D.strokeOval(100, 100, 300, 300);
-
-        graphicsContext2D.setStroke(Color.BLACK);
-        graphicsContext2D.strokeLine(100, 100, 400, 400);
-
-        graphicsContext2D.setFill(Color.BLACK);
-        graphicsContext2D.fillPolygon(
-                new double[] {0, 100, 200, 100, 0},
-                new double[] {200, 100, 100, 200, 200},
-                4
-        );
-
-        canvas.setOnMousePressed( evt -> {
-            GraphicsContext g = canvas.getGraphicsContext2D();
-            double red = evt.getY() / HEIGHT;
-            double blue = evt.getX() / WIDTH;
-            double green = Math.random();
-            if (evt.isShiftDown()) {
-                g.setFill(Color.color(red, green, blue));
-                g.fillOval(evt.getX(), evt.getY(), 60, 30);
-            } else {
-                g.setFill(Color.color(red, green, blue));
-                g.fillRect(evt.getX(), evt.getY(), 60, 30);
-            }
-        });
-
-        VBox vbox = new VBox(canvas);
-        Scene scene = new Scene(vbox);
         stage.setScene(scene);
         stage.show();
     }
